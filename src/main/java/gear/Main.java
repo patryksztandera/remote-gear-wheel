449ebf6be5450277482ab757696d4a2717a3e65f
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-03-16 17:52:43 +0100

    Changed output file name

--- src/main/java/gear/Main.java
+++ src/main/java/gear/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         String inputFilePath = "daneWejsciowe.json";
-        String outputFilePath = "daneWyjscioweTest.json";
+        String outputFilePath = "daneWyjsciowe.json";
         PrintWriter clearFile = new PrintWriter(outputFilePath);
         clearFile.close();
 
